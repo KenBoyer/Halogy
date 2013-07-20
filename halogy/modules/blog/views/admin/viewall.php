6ccf18a5b89f2289258c6d0498f30bcb75d13bb5
Ken Boyer <ken.boyer2@gmail.com>
2013-07-20 11:38:15 -0400

    Formatting changes.

--- halogy/modules/blog/views/admin/viewall.php
+++ halogy/modules/blog/views/admin/viewall.php
@@ -15,8 +15,8 @@
 <table class="default clear">
 	<tr>
 		<th><?php echo order_link('/admin/blog/viewall','posttitle','Post'); ?></th>
-		<th><?php echo order_link('/admin/blog/viewall','datecreated','Date'); ?></th>
-		<th class="narrow"><?php echo order_link('/admin/blog/viewall','published','Published'); ?></th>
+		<th><?php echo order_link('/admin/blog/viewall','datecreated','Date Created'); ?></th>
+		<th class="narrow"><?php echo order_link('/admin/blog/viewall','published','Status'); ?></th>
 		<th class="tiny">&nbsp;</th>
 		<th class="tiny">&nbsp;</th>
 	</tr>
@@ -25,19 +25,20 @@
 		<td><?php echo (in_array('blog_edit', $this->permission->permissions)) ? anchor('/admin/blog/edit_post/'.$post['postID'], $post['postTitle']) : $post['postTitle']; ?></td>
 		<td><?php echo dateFmt($post['dateCreated'], '', '', TRUE); ?></td>
 		<td>
-			<?php
-				if ($post['published']) echo '<span style="color:green;">Yes</span>';
-				else echo 'No';
-			?>
+			<?php if ($post['published']): ?>
+				<span class="label label-success">Published</span>
+			<?php else: ?>
+				<span class="label">Draft</span>
+			<?php endif; ?>
 		</td>
 		<td class="tiny">
 			<?php if (in_array('blog_edit', $this->permission->permissions)): ?>
-				<?php echo anchor('/admin/blog/edit_post/'.$post['postID'], 'Edit'); ?>
+				<?php echo anchor('/admin/blog/edit_post/'.$post['postID'], 'Edit <i class="icon-edit"></i>', 'class="btn btn-info"'); ?>
 			<?php endif; ?>
 		</td>
 		<td class="tiny">			
 			<?php if (in_array('blog_delete', $this->permission->permissions)): ?>
-				<?php echo anchor('/admin/blog/delete_post/'.$post['postID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
+				<?php echo anchor('/admin/blog/delete_post/'.$post['postID'], 'Delete <i class="icon-trash"></i>', array('onclick' => 'return confirm(\'Are you sure you want to delete this?\')', 'class' => 'btn btn-danger')); ?>
 			<?php endif; ?>
 		</td>
 	</tr>
