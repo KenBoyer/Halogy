2953d8856daf94087d2b28751b5082c5455a7bc5
Ken Boyer <ken.boyer2@gmail.com>
2013-07-20 11:36:23 -0400

    More formatting changes.

--- halogy/modules/blog/views/admin/comments.php
+++ halogy/modules/blog/views/admin/comments.php
@@ -14,7 +14,7 @@
 		<th>Email</th>
 		<th>Comment</th>	
 		<th>Status</th>
-		<th class="tiny">&nbsp;</th>
+		<th>&nbsp;</th>
 		<th class="tiny">&nbsp;</th>
 	</tr>
 <?php foreach ($comments as $comment): ?>
@@ -24,21 +24,21 @@
 		<td><?php echo $comment['fullName']; ?></td>
 		<td><?php echo $comment['email']; ?></td>
 		<td><small><?php echo (strlen($comment['comment'] > 50)) ? htmlentities(substr($comment['comment'], 0, 50)).'...' : htmlentities($comment['comment']); ?></small></td>						
-		<td><?php echo ($comment['active']) ? '<span style="color:green"><strong>Active</strong></span>' : '<span style="color:orange;"><strong>Pending</strong></span>'; ?></td>		
+		<td><?php echo ($comment['active']) ? '<span class="label label-success">Active</span>' : '<span class="label label-warning">Pending</span>'; ?></td>
 		<td>
 		<?php
 			if (!$comment['active'])
 			{
-				echo anchor('/admin/blog/approve_comment/'.$comment['commentID'], 'Approve');
+				echo anchor('/admin/blog/approve_comment/'.$comment['commentID'], 'Approve <i class="icon-check"></i>', 'class="btn btn-success"');
 			}
 			else
 			{
-				echo anchor('/admin/blog/deactivate_comment/'.$comment['commentID'], 'Deactivate');
+				echo anchor('/admin/blog/deactivate_comment/'.$comment['commentID'], 'Deactivate <i class="icon-off"></i>', 'class="btn btn-warning"');
 			}
 		?>
 		</td>
 		<td>
-			<?php echo anchor('/admin/blog/delete_comment/'.$comment['commentID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
+			<?php echo anchor('/admin/blog/delete_comment/'.$comment['commentID'], 'Delete <i class="icon-trash"></i>', array('onclick' => 'return confirm(\'Are you sure you want to delete this?\')', 'class' => 'btn btn-danger')); ?>
 		</td>
 	</tr>
 <?php endforeach; ?>
